3b0517d17083d19e365d55253a1cfc9d654f727e
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-07-15 12:26:19 -0400

    build: Initial renaming and setup as parent project

---
+++ lib/kiba/pastperfect_we.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require "kiba/extend"
+require "yaml"
+require "zeitwerk"
+
+# Needed because these are extended before Zeitwerk code loading is set up
+require_relative "pastperfect_we/base_config"
+require_relative "pastperfect_we/util_config"
+
+# dev
+require "pry"
+
+module Kiba
+  # Application loading, base configuration, and top-level convenience methods
+  module PastperfectWe
+    ::Ppwe = Kiba::PastperfectWe
+
+    module_function
+
+    extend Dry::Configurable
+
+    extend Ppwe::UtilConfig
+    set_up_util_config
+
+    extend Ppwe::BaseConfig
+
+    def loader
+      @loader ||= setup_loader
+    end
+
+    def setup_loader
+      puts "LOADING KIBA-PPWE" if verbose?
+      base_config
+      @loader = Zeitwerk::Loader.new
+      @loader.collapse(File.join(__dir__, "pastperfect_we", "config"))
+      @loader.push_dir(
+        File.expand_path(__FILE__).delete_suffix(".rb"),
+        namespace: Kiba::PastperfectWe
+      )
+      @loader.inflector.inflect(
+        "version" => "VERSION"
+      )
+      jobs = File.join(__dir__, "pastperfect_we", "jobs")
+      transforms = File.join(__dir__, "pastperfect_we", "transforms")
+      @loader.do_not_eager_load(jobs, transforms)
+      @loader.enable_reloading
+      @loader.setup
+      @loader
+    end
+    private_class_method(:setup_loader)
+
+    def reload! = @loader.reload
+
+    def configs = Kiba::Extend.project_configs
+  end
+end
+
+Kiba::PastperfectWe.loader
+Kiba::Extend.config.config_namespaces << Kiba::PastperfectWe
---
+++ lib/kiba/pastperfect_we/util_config.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    # Mixin module to organize config settings controlling how jobs are
+    #  run, screen output, etc.
+    module UtilConfig
+      def set_up_util_config(from_extended = false)
+        # For dev/debugging. Set to :verbose to have application print
+        #   everything it does to screen. We set this in Kiba::Extend because
+        #   a lot of the output behavior is in that library.
+        Kiba::Extend.config.job_verbosity = :normal
+
+        # We then create a CSU-level setting that inherits the default value
+        #   of Kiba::Extend.job_verbosity, on which we can build
+        #   CSU-project-specific functionality
+        setting :stdout_mode, default: Kiba::Extend.job_verbosity,
+          reader: true
+
+        # Used by Ppwe::RegistryData#register to register directories of files.
+        #   Hash keys should be directory names (living under `datadir` for
+        #   project) and Hash value will be the registry namespace the files
+        #   are registered in.
+        setting :auto_register_dirs,
+          default: {
+            "orig" => "orig"
+          },
+          reader: true
+
+        #################################################################
+        # Leave it alone or suffer
+        #################################################################
+        # @return [Kiba::Extend::FileRegistry]
+        setting :registry, default: Kiba::Extend.registry, reader: true
+
+        # This supports protection against errors due to unexpected empty job
+        #   output or missing tables
+        # @return [Array]
+        setting :blank_jobs, default: %i[], reader: true
+      end
+
+      def verbose?
+        true if %i[verbose debug].include?(stdout_mode)
+      end
+
+      def debug?
+        true if stdout_mode == :debug
+      end
+
+      # methods to delete after development is done
+
+      # @param jobkey [Symbol]
+      # @param column [Symbol] keycolumn on which to lookup
+      def get_lookup(jobkey:, column:)
+        return nil if Ppwe.blank_jobs.include?(jobkey)
+        return nil unless job_output?(jobkey)
+
+        Kiba::Extend::Utils::Lookup.csv_to_hash(
+          file: Ppwe.registry.resolve(jobkey).path,
+          keycolumn: column
+        )
+      end
+
+      # @param jobkey [Symbol]
+      def job_output?(jobkey)
+        return false if Ppwe.blank_jobs.include?(jobkey)
+
+        result = Kiba::Extend::Job.output?(jobkey)
+        Ppwe.blank_jobs << jobkey unless result == true
+        result
+      end
+
+      # @param jobkey [Symbol]
+      # @return [Array<Symbol>]
+      def headers_for(jobkey)
+        path = Ppwe.registry.resolve(jobkey).path
+        return [] unless File.exist?(path)
+
+        `head -n 1 #{path}`.chomp
+          .split(",")
+          .map(&:to_sym)
+      end
+    end
+  end
+end
