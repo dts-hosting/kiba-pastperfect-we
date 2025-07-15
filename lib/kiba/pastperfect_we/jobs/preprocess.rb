09829163b7db2461f5d7e2ca5011688e3a730c98
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-07-15 19:20:49 -0400

    feat: Create the configurable Preprocess job

---
+++ lib/kiba/pastperfect_we/jobs/preprocess.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      # Deletes fields/columns where all values are blank, and deletes rows
+      #  where only the first field/column is non-blank
+      module Preprocess
+        module_function
+
+        def job(source:, dest:)
+          Kiba::Extend::Jobs::Job.new(
+            files: {
+              source: source,
+              destination: dest
+
+            },
+            transformer: xforms
+          )
+        end
+
+        def xforms
+          Kiba.job_segment do
+            transform Delete::EmptyFields
+
+            transform do |row|
+              chk = row.dup
+              chk.shift
+              row unless chk.all? { |field, val| val.blank? }
+            end
+          end
+        end
+      end
+    end
+  end
+end
