93c65c6a3f2628cf94e7b8062f64e23dfee45498
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-07-16 17:29:58 -0400

    feat: Add MergeTable transform

---
+++ lib/kiba/pastperfect_we/transforms/merge_table.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Transforms
+      # Merge the source table given in this transform into the source(s)
+      #  specified in the job definition from where it is called
+      # @note Use this transform when you need to merge most/all of the
+      #   fields in the source table into the calling/target table. If you
+      #   only need to merge one or a few fields into the calling/target
+      #   table, it'll be less typing to use the kiba-extend
+      #   Merge::MultiRowLookup transform directly.
+      class MergeTable
+        # @param source [Symbol] job key for merge source table
+        # @param join_column [Symbol] field in calling/target table containing
+        #   value to match against lookup_on column in source table
+        # @param drop_fields [Array<Symbol] fields NOT to merge from the source
+        #   table. NOTE: known/registered ID fields and created/modified by/date
+        #   fields are automatically excluded
+        # @param opts [nil, Hash] of additional kiba-extend
+        #   Merge::MultiRowLookup parameters to pass through. The following
+        #   parameters cannot be passed through: lookup, keycolumn, fieldmap
+        def initialize(source:, join_column:, drop_fields: [], opts: nil)
+          @source = source
+          @join_column = join_column
+          @drop = drop_fields
+          @opts = opts
+          @lookup = Ppwe.get_lookup(
+            jobkey: source, column: Ppwe.lookup_on_for(source)
+          )
+          @merger = Merge::MultiRowLookup.new(**build_merge_transform_opts)
+        end
+
+        def process(row)
+          merger.process(row)
+          row
+        end
+
+        private
+
+        attr_reader :source, :join_column, :drop, :opts, :lookup, :merger
+
+        def build_merge_transform_opts
+          base = {
+            lookup: lookup,
+            keycolumn: join_column,
+            fieldmap: build_merge_map
+          }
+          return base unless opts
+
+          base.merge(clean_opts)
+        end
+
+        def build_merge_map
+          Ppwe.mergeable_headers_for(
+            :prep__person_biographical_information, drop: drop
+          ).map { |field| [field, field] }
+            .to_h
+        end
+
+        def clean_opts
+          forbidden = %i[lookup keycolumn fieldmap]
+          return opts unless opts.keys.any? { |opt| forbidden.include?(opt) }
+
+          forbidden.each { |opt| opts.delete(opt) }
+          opts
+        end
+      end
+    end
+  end
+end
