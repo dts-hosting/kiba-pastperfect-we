a13d3b1adb50ba6b2c43b0f4b58078a241a6b8c7
livsolis <livsolis@utexas.edu>
2025-07-17 17:01:51 -0500

    more scripts

---
+++ lib/kiba/pastperfect_we/jobs/prep/activity.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module Activity
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest,
+                lookup: :preprocess__dictionary_item
+
+              },
+              transformer: xforms
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Ppwe::Transforms::DictionaryLookup,
+                fields: %i[donebyid activityid]
+            end
+          end
+        end
+      end
+    end
+  end
+end
