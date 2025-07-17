7c91dfadfa968143328a248dfeb285f39a17e7bc
livsolis <livsolis@utexas.edu>
2025-07-17 15:24:41 -0500

    Create accession.rb

---
+++ lib/kiba/pastperfect_we/jobs/prep/accession.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module Accession
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest,
+                lookup: :preprocess__dictionary_item
+
+              },
+              transformer: xforms
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Ppwe::Transforms::DictionaryLookup,
+                fields: %i[statusid receivedasid interumlocationid receivedbyid
+                  accessionedbyid renewedbyid]
+
+              %i[displayrestrictedflagforcatalogitems isremoved].each do |field|
+                transform Replace::FieldValueWithStaticMapping,
+                  source: field,
+                  mapping: Ppwe.boolean_yes_no_mapping
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
