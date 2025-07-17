7488cbc5c854a425c56aa99354e82b8af87eb24d
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-07-17 17:52:47 -0400

    feat: Merge PersonUrl into Person combined

--- lib/kiba/pastperfect_we/jobs/person/combined.rb
+++ lib/kiba/pastperfect_we/jobs/person/combined.rb
@@ -11,7 +11,8 @@ module Kiba
             Kiba::Extend::Jobs::Job.new(
               files: {
                 source: :prep__person,
-                destination: :person__combined
+                destination: :person__combined,
+                lookup: :prep__person_url
               },
               transformer: xforms
             )
@@ -19,12 +20,25 @@ module Kiba
 
           def xforms
             Kiba.job_segment do
+              # OLIVIA
+              # This job definition clearly shows the distinction I was trying
+              #   to make in our meeting today about MergeTable (for merging in
+              #   all or most of a table) vs. Merge::MultiRowLookup (for merging
+              #   in just a few fields)
               transform Ppwe::Transforms::MergeTable,
                 source: :prep__person_biographical_information,
                 join_column: :id,
                 drop_fields: %i[maritalstatus],
                 opts: {null_placeholder: "FOO",
                        constantmap: {biomerged: "y"}}
+
+              transform Merge::MultiRowLookup,
+                lookup: prep__person_url,
+                keycolumn: :id,
+                fieldmap: {
+                  url: :url_name,
+                  url_display: :url_displayname
+                }
             end
           end
         end
