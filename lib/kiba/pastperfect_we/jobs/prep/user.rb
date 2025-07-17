7e51467bc77ccfcb67375e8e70ab7fb2c0b0e45f
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-07-17 16:28:53 -0400

    feat: Add prep__user job

---
+++ lib/kiba/pastperfect_we/jobs/prep/user.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module User
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest,
+                lookup: :preprocess__user_role
+              },
+              transformer: xforms
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Ppwe::Transforms::DictionaryLookup,
+                fields: %i[museumid]
+
+              transform Merge::MultiRowLookup,
+                lookup: preprocess__user_role,
+                keycolumn: :userroleid,
+                fieldmap: {userrole: :name}
+              transform Delete::Fields, fields: :userroleid
+
+              %i[isactive isremoved].each do |field|
+                transform Replace::FieldValueWithStaticMapping,
+                  source: field,
+                  mapping: Ppwe.boolean_yes_no_mapping
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
