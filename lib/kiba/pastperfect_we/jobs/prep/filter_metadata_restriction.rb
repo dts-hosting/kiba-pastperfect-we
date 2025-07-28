0f1840e59d9b4ef477d54a1ab9837663b6ebe36e
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-07-28 17:17:02 -0400

    feat: Add prep__filter_metadata_restriction job
    I can't find where to look up the :restrictionid, :objecttype, or
    :fieldtype values merged in from the FilterMetadata table.
    
    Also this table has 235 rows, whereas the FilterMetadata table has
    1011 rows. FilterMetadata contains other kind of filters (check the
    parameters that contain "defaultview" or "enum" or just the name of
    another table. But it also has lots of rows for fields that don't seem
    like they should ever be filtered/controlled.

---
+++ lib/kiba/pastperfect_we/jobs/prep/filter_metadata_restriction.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module FilterMetadataRestriction
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest
+              },
+              transformer: xforms
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Ppwe::Transforms::MergeTable,
+                source: :preprocess__filter_metadata,
+                join_column: :metadataid
+            end
+          end
+        end
+      end
+    end
+  end
+end
