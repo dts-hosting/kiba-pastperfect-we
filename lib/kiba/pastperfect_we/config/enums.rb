0d43e877ff5c63e5b1f17c4ba7c483342b930d3d
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-09-16 15:17:02 -0400

    feat: Add enums from hardcoded values spreadsheet

---
+++ lib/kiba/pastperfect_we/config/enums.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    # Mappings of numeric enum values stored in the database to human-usable
+    #   values. We HOPE these are going to be the same across the board, for
+    #   all PPWE instance. If not, we got problems... 🤞
+    module Enums
+      module_function
+
+      extend Dry::Configurable
+
+      # @return [Hash{String=>String}] Used in CatalogItem.ItemType
+      setting :item_type, reader: true, default: {
+        "1" => "object",
+        "2" => "photo",
+        "3" => "archives",
+        "4" => "library",
+        "5" => "deaccessioned"
+      }
+
+      # @return [Hash{String=>String}] Used in Accession.AccessionType
+      setting :accession_type, reader: true, default: {
+        "1" => "accession",
+        "2" => "loan, returned",
+        "3" => "temporary custody",
+        "4" => "temporary custody, returned",
+        "5" => "loan, incoming"
+      }
+
+      # @return [Hash{String=>String}] Used in
+      #   AccessionInsuranceInformation.InsuredBy,
+      #   AccessionShippingInformation.TransportationCostPaidBy,
+      #   LoanInsuranceInformation.InsuredBy,
+      #   LoanShippingInformation.TransportationCostPaidBy,
+      #   ExhibitInsuranceInformation.InsuredBy,
+      #   ExhibitShippingInformation.TransportationCostPaidBy
+      setting :responsible_party, reader: true, default: {
+        "0" => nil, # displays as "None Selected"
+        "1" => "borrower",
+        "2" => "lender"
+      }
+
+      # @return [Hash{String=>String}] Used in
+      #   ContactAddressAndPhoneNumbers.primaryphonenumbertypeid,
+      #   ContactAddressAndPhoneNumbers.secondaryphonenumbertypeid,
+      #   ContactAddressAndPhoneNumbers.otherphonenumber1typeid,
+      #   ContactAddressAndPhoneNumbers.otherphonenumber2typeid,
+      #   LoanContactInformation.primaryphonenumbertype,
+      #   LoanContactInformation.secondaryphonenumbertype,
+      #   LoanContactInformation.otherphonenumbertype
+      setting :phone_number_type, reader: true, default: {
+        "0" => nil, # displays as "None of the Above"
+        "1" => "home",
+        "2" => "mobile",
+        "3" => "work",
+        "4" => "fax"
+      }
+    end
+  end
+end
