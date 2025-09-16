dbbe85eb29636338536fe6287b88b879c0816037
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-09-16 19:00:42 -0400

    feat: Add prep__exhibit_insurance_information job

---
+++ lib/kiba/pastperfect_we/jobs/prep/exhibit_insurance_information.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module ExhibitInsuranceInformation
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest,
+                lookup: %i[]
+              },
+              transformer: Ppwe::Prep.get_xforms(self)
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Replace::FieldValueWithStaticMapping,
+                source: :insuredby,
+                mapping: Ppwe::Enums.responsible_party,
+                delete_source: false,
+                fallback_val: nil
+            end
+          end
+        end
+      end
+    end
+  end
+end
