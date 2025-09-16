d34877d3bb3e8637aa43bb6ef7a71af69f80c489
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-09-16 19:33:13 -0400

    feat: Add prep__accession_insurance_information job

---
+++ lib/kiba/pastperfect_we/jobs/prep/accession_insurance_information.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module AccessionInsuranceInformation
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest,
+                lookup: %i[]
+              },
+              transformer: Ppwe::Prep.get_xforms(self)
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Replace::FieldValueWithStaticMapping,
+                source: :insuredby,
+                mapping: Ppwe::Enums.responsible_party,
+                delete_source: false,
+                fallback_val: nil
+              transform Clean::RegexpFindReplaceFieldVals,
+                fields: :insuredvalue,
+                find: /\$/,
+                replace: ""
+            end
+          end
+        end
+      end
+    end
+  end
+end
