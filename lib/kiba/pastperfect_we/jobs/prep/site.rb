f846280e7767487ce15e642a28b938407029f5b1
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-09-19 13:32:48 -0400

    feat: Add prep__site job

---
+++ lib/kiba/pastperfect_we/jobs/prep/site.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module Site
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest
+              },
+              transformer: Ppwe::Prep.get_xforms(self)
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              %i[ispublicaccess isremoved].each do |field|
+                transform Replace::FieldValueWithStaticMapping,
+                  source: field,
+                  mapping: Ppwe.boolean_yes_no_mapping
+              end
+
+              transform Ppwe::Transforms::DictionaryLookup,
+                fields: %i[countryid]
+            end
+          end
+        end
+      end
+    end
+  end
+end
