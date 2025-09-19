22756be9a4918f89ee3c03890c36e36d9b17a96a
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-09-19 13:53:39 -0400

    feat: Add term_types and loose_fields settings with initial values

--- lib/kiba/pastperfect_we/config/terms.rb
+++ lib/kiba/pastperfect_we/config/terms.rb
@@ -2,16 +2,26 @@
 
 module Kiba
   module PastperfectWe
+    # Config module for settings and functionality related to term field
+    #   identification and term uniqueness
+    #
+    # "Terms" here means values that get treated as some type of controlled
+    #   value in target systems.
     module Terms
       module_function
 
       extend Dry::Configurable
+
 
       # @return [String] value inserted between "term-like value" used in a
       #   record and its Table.Id source indication
       setting :term_source_prefix,
         reader: true,
         default: " termsrc:"
+
+      setting :term_types,
+        reader: true,
+        default: %i[location misc name place subject]
 
       # @return [Hash<String => Symbol>] keys are table names; values are the
       #   fields from PREP jobs that contain the "term-like" values merged into
@@ -25,6 +35,18 @@ module Kiba
           "Person" => :fullname,
           "Site" => :sitenumberandname,
           "User" => :fullname
+        }
+
+      # Fields that do not lookup from other tables and are probably
+      #   freetext, but that contain predominantly values that become
+      #   terms in target systems
+      # @return [Hash] where keys are term_types values
+      setting :loose_fields,
+        reader: true,
+        default: {
+          name: [
+            {table: "ArchiveIdentity", field: :creatoraddedentry}
+          ]
         }
     end
   end
