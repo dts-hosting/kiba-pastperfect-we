b4452bbe066eda72b29ea0017c819edc9800e3d3
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-09-19 15:25:19 -0400

    feat: Add prep__catalog_item_geology job

---
+++ lib/kiba/pastperfect_we/jobs/prep/catalog_item_geology.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module CatalogItemGeology
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest,
+                lookup: %i[
+                  prep__site
+                ]
+              },
+              transformer: Ppwe::Prep.get_xforms(self)
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Ppwe::Transforms::DictionaryLookup,
+                fields: %i[rocktypeid hardnessid lusterid crystalsystemid
+                  originid fossilsid grainsizeid pressureid
+                  classificationid temperatureid colorid
+                  surfaceprocessid collectorid identifiedbyid]
+
+              transform Merge::MultiRowLookup,
+                lookup: prep__site,
+                keycolumn: :siteid,
+                fieldmap: {sitename: :sitename}
+              transform Delete::Fields,
+                fields: %i[siteid]
+            end
+          end
+        end
+      end
+    end
+  end
+end
