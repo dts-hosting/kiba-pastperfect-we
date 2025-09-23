a45a4714f2ce1d9d4722cdbded76c1aa33a5a9f8
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-09-23 15:27:38 -0400

    bugfix: Get prep__catalog_item_lexicon values from prep__lexicon_item

--- lib/kiba/pastperfect_we/jobs/prep/catalog_item_lexicon.rb
+++ lib/kiba/pastperfect_we/jobs/prep/catalog_item_lexicon.rb
@@ -12,18 +12,36 @@ module Kiba
               files: {
                 source: source,
                 destination: dest,
-                lookup: %i[
-                  prep__user
-                ]
+                lookup: get_lookups
               },
               transformer: Ppwe::Prep.get_xforms(self)
             )
           end
 
+          def get_lookups
+            return [] if Ppwe.mode == :migration
+
+            [:prep__lexicon_item]
+          end
+
           def xforms
             Kiba.job_segment do
-              transform Ppwe::Transforms::DictionaryLookup,
-                fields: %i[itemname2id itemname3id]
+              if Ppwe.mode == :review
+                lookup_fields = %i[itemname2id itemname3id]
+
+                lookup_fields.each do |field|
+                  target = field.to_s
+                    .sub("item", "object")
+                    .delete_suffix("id")
+                    .to_sym
+                  transform Merge::MultiRowLookup,
+                    lookup: prep__lexicon_item,
+                    keycolumn: field,
+                    fieldmap: {target => :objectname}
+                end
+                transform Delete::Fields,
+                  fields: lookup_fields
+              end
             end
           end
         end
