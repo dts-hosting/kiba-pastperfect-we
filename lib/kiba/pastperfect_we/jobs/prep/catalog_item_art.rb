b3784a42c6d141a471725682c88984710c1877db
livsolis <livsolis@utexas.edu>
2025-09-25 16:53:11 -0500

    Misc CatalogItem tables + art
    Adding assundry catalogItem tables that needed prep, plus the prep for Art tab of Object records.

---
+++ lib/kiba/pastperfect_we/jobs/prep/catalog_item_art.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module CatalogItemArt
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest,
+                lookup: %i[
+                  prep__person
+                ]
+              },
+              transformer: Ppwe::Prep.get_xforms(self)
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Ppwe::Transforms::DictionaryLookup,
+                fields: %i[cultureid schoolid creatorroleid creator2roleid
+                  creator3roleid imagesizeid framesizeid collectorid]
+
+              transform Merge::MultiRowLookup,
+                lookup: prep__person,
+                keycolumn: :creatorid,
+                fieldmap: {creator: :fullname}
+
+              transform Merge::MultiRowLookup,
+                lookup: prep__person,
+                keycolumn: :creator2id,
+                fieldmap: {creator2: :fullname}
+
+              transform Merge::MultiRowLookup,
+                lookup: prep__person,
+                keycolumn: :creator3id,
+                fieldmap: {creator3: :fullname}
+
+              transform Delete::Fields,
+                fields: %i[creatorid creator2id creator3id]
+            end
+          end
+        end
+      end
+    end
+  end
+end
