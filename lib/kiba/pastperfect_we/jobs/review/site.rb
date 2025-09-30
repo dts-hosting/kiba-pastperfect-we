610162a2b54a9219704b60173150536096aa5385
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-09-30 12:50:37 -0400

    feat: Add review__site job

---
+++ lib/kiba/pastperfect_we/jobs/review/site.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Review
+        module Site
+          module_function
+
+          def job
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: :prep__site,
+                destination: :review__site
+              },
+              transformer: xforms
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Ppwe::Transforms::DeleteTermSourceIndication,
+                table: "Site"
+
+              transform Ppwe::Transforms::MergeTable,
+                source: :prep__site_archeology_details,
+                join_column: :id,
+                delete_join_column: false
+
+              transform Ppwe::Transforms::MergeTable,
+                source: :prep__site_mapping_options,
+                join_column: :id,
+                delete_join_column: false
+            end
+          end
+        end
+      end
+    end
+  end
+end
