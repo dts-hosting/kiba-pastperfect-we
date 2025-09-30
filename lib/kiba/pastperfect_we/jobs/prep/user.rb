73735d798b29cf26249101539e42d2ade878d598
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-09-29 20:59:17 -0400

    bugfix: Remove lookup of museumid from DictionaryItem in jobs

--- lib/kiba/pastperfect_we/jobs/prep/user.rb
+++ lib/kiba/pastperfect_we/jobs/prep/user.rb
@@ -20,9 +20,6 @@ module Kiba
 
           def xforms
             Kiba.job_segment do
-              transform Ppwe::Transforms::DictionaryLookup,
-                fields: %i[museumid]
-
               transform Merge::MultiRowLookup,
                 lookup: preprocess__user_role,
                 keycolumn: :userroleid,
