bc0a4b2d71d8bbdbfc8383834dcd99dd76cf72ae
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-01 20:37:25 -0400

    feat: Prepend contextual fieldname prefixes to loan info tables

--- lib/kiba/pastperfect_we/jobs/prep/loan_insurance_information.rb
+++ lib/kiba/pastperfect_we/jobs/prep/loan_insurance_information.rb
@@ -24,6 +24,15 @@ module Kiba
                 mapping: Ppwe::Enums.responsible_party,
                 delete_source: false,
                 fallback_val: nil
+              prefix_needed = %i[representative phonenumber premium]
+              to_prefix = prefix_needed.intersection(
+                Ppwe.mergeable_headers_for(
+                  :preprocess__loan_insurance_information
+                )
+              )
+              prefix_mapping = to_prefix.map { |f| [f, :"insurance#{f}"] }
+                .to_h
+              transform Rename::Fields, fieldmap: prefix_mapping
             end
           end
         end
--- lib/kiba/pastperfect_we/jobs/prep/loan_shipping_information.rb
+++ lib/kiba/pastperfect_we/jobs/prep/loan_shipping_information.rb
@@ -27,6 +27,18 @@ module Kiba
               transform Delete::FieldValueMatchingRegexp,
                 fields: :noofcrates,
                 match: /^0$/
+
+              prefix_needed = %i[arrivedate trackingnumber handlers
+                primarycarrier additionalcarriers cratetypes
+                noofcrates cratelist]
+              to_prefix = prefix_needed.intersection(
+                Ppwe.mergeable_headers_for(
+                  :preprocess__loan_shipping_information
+                )
+              )
+              prefix_mapping = to_prefix.map { |f| [f, :"shipping#{f}"] }
+                .to_h
+              transform Rename::Fields, fieldmap: prefix_mapping
             end
           end
         end
