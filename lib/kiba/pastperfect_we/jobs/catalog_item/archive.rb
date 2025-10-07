4af1d69056e689c52bd03445d426eff6fe2f872f
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-07 17:42:33 -0400

    docs: Add notes re: archive_container_location -> catalog_item__archive merge

--- lib/kiba/pastperfect_we/jobs/catalog_item/archive.rb
+++ lib/kiba/pastperfect_we/jobs/catalog_item/archive.rb
@@ -11,7 +11,11 @@ module Kiba
             Kiba::Extend::Jobs::Job.new(
               files: {
                 source: :catalog_item__base,
-                destination: :catalog_item__archive
+                destination: :catalog_item__archive,
+                lookup: {
+                  jobkey: :prep__archive_container_location,
+                  lookup_on: :catalogitemid
+                }
               },
               transformer: xforms
             )
@@ -32,6 +36,23 @@ module Kiba
                 join_column: :catalogitemid,
                 delete_join_column: false
 
+              # # I thought we discussed yesterday that the nature of
+              # #   ArchiveContainerLocation made it unsuitable for
+              # #   merge into this table, and that it would be a
+              # #   separate catalog_item__archive_container_list
+              # #   table? If that is still the case, I would delete
+              # #   all the commented out code still in this file. If
+              # #   you changed your mind on that point, delete the
+              # #   Count::MatchingRowsInLookup,
+              # #   Delete::FieldValueMatchingRegexp, and
+              # #   Rplace::EmptyValues transforms I added, and
+              # #   uncomment the remaining commented stuff. THOUGH,
+              # #   this is another where the actual lookup id is and
+              # #   should remain :id, but we need to merge here on
+              # #   :catalogitemid. So you will need to change
+              # #   MergeTable to a Merge::MultiRowLookup (so keep the
+              # #   lookup file definition above)
+
               # drop_fields = %i[catalogitemid id position]
               # transform Ppwe::Transforms::MergeTable,
               #   source: :prep__archive_container_location,
@@ -39,13 +60,22 @@ module Kiba
               #   delete_join_column: false,
               #   drop_fields: drop_fields
 
+              transform Count::MatchingRowsInLookup,
+                lookup: prep__archive_container_location,
+                keycolumn: :catalogitemid,
+                targetfield: :containerlistcount
+              transform Delete::FieldValueMatchingRegexp,
+                fields: :containerlistcount,
+                match: /^0$/
+
               content_fields = Ppwe.mergeable_headers_for(
                 :prep__archive_allied_materials
               ) + Ppwe.mergeable_headers_for(
                 :prep__archive_identity
               ) + Ppwe.mergeable_headers_for(
                 :prep__archive_structure
-              )
+              ) + [:containerlistcount]
+
               # + Ppwe.mergeable_headers_for(
               #   :prep__archive_container_location, drop: drop_fields
               # )
@@ -53,6 +83,10 @@ module Kiba
               transform FilterRows::AnyFieldsPopulated,
                 action: :keep,
                 fields: content_fields
+
+              transform Replace::EmptyFieldValues,
+                fields: :containerlistcount,
+                value: "0"
             end
           end
         end
