f87a0deaa3e28c3489854e813a3609d3ee32f7fa
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-07 19:35:35 -0400

    feat: Add prep__inventory_history

---
+++ lib/kiba/pastperfect_we/jobs/prep/inventory_history.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module InventoryHistory
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest
+              },
+              transformer: Ppwe::Prep.get_xforms(self)
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Ppwe::Transforms::DictionaryLookup,
+                fields: %i[lastinventoriedbyid]
+
+              transform Sort::ByFieldValue, field: :catalogitemid
+            end
+          end
+        end
+      end
+    end
+  end
+end
