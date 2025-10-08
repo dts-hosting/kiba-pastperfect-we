2f060d8a7c910bbf71337a6741a888660d3eba7e
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-07 22:01:02 -0400

    feat: Add prep__exhibit_image
    Resolves #66

---
+++ lib/kiba/pastperfect_we/jobs/prep/exhibit_image.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module ExhibitImage
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest
+              },
+              transformer: Ppwe::Prep.get_xforms(self)
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Ppwe::Transforms::MergeTable,
+                source: :exhibit__target_system_lookup,
+                join_column: :exhibitid,
+                delete_join_column: false
+            end
+          end
+        end
+      end
+    end
+  end
+end
