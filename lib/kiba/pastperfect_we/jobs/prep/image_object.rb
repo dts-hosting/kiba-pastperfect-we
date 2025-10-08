40e6330d3f60f61ff94d0b9631ddf74a84a3dda9
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-07 20:32:57 -0400

    feat: Finalize prep__image_object

--- lib/kiba/pastperfect_we/jobs/prep/image_object.rb
+++ lib/kiba/pastperfect_we/jobs/prep/image_object.rb
@@ -24,15 +24,34 @@ module Kiba
                 fields: %i[originalobjectformatid equipmenttoacquireid
                   softwaretoacquireid grayscaleorrgbid]
 
+              %i[isrestricted ispublic].each do |field|
+                transform Replace::FieldValueWithStaticMapping,
+                  source: field,
+                  mapping: Ppwe.boolean_yes_no_mapping
+              end
+
               transform Merge::MultiRowLookup,
                 lookup: prep__user,
                 keycolumn: :createdbyid,
                 fieldmap: {createdby: Ppwe::Terms.table_config["User"]}
 
               transform Delete::Fields,
-                fields: :createdbyid
+                fields: %i[createdbyid extralargeimagefileobjectid
+                  miniaturesquarefileobjectid
+                  miniaturenativefileobjectid
+                  smallimagefileobjectid
+                  mediumimagefileobjectid
+                  largeimagefileobjectid]
+              transform Clean::RegexpFindReplaceFieldVals,
+                fields: :azimuth,
+                find: /^0$/,
+                replace: ""
 
-              transform Delete::EmptyFields
+              transform Ppwe::Transforms::MergeTable,
+                source: :prep__file_object,
+                join_column: :originalfileobjectid,
+                drop_fields: %i[museumid],
+                merged_field_prefix: "original"
             end
           end
         end
