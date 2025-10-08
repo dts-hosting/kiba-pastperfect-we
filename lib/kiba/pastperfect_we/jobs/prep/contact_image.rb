81d67c8f85f2597ac6024334a29e7e7e9a185526
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-08 11:11:28 -0400

    feat: Add prep__contact_image
    Resolves #72

---
+++ lib/kiba/pastperfect_we/jobs/prep/contact_image.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module ContactImage
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest,
+                lookup: :prep__contact
+              },
+              transformer: Ppwe::Prep.get_xforms(self)
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Merge::MultiRowLookup,
+                lookup: prep__contact,
+                keycolumn: :contactid,
+                fieldmap: {contactname: Ppwe::Terms.table_config["Contact"]}
+            end
+          end
+        end
+      end
+    end
+  end
+end
