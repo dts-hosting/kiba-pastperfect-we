b9e278bb5977b328136666794b2f965726dba9df
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-07 21:11:57 -0400

    feat: Add prep__condition_report_image
    Resolves #67

---
+++ lib/kiba/pastperfect_we/jobs/prep/condition_report_image.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module ConditionReportImage
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest
+              },
+              transformer: Ppwe::Prep.get_xforms(self)
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Ppwe::Transforms::MergeTable,
+                source: :condition_report__target_system_lookup,
+                join_column: :conditionreportid,
+                delete_join_column: false
+            end
+          end
+        end
+      end
+    end
+  end
+end
