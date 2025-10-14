bc78b93fe5cb2737a96677fe3f7fcc2fafbc3dde
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-14 17:16:03 -0400

    feat: Add numberofcatalogitems to review__outgoing_loan

--- lib/kiba/pastperfect_we/jobs/review/outgoing_loan.rb
+++ lib/kiba/pastperfect_we/jobs/review/outgoing_loan.rb
@@ -12,9 +12,12 @@ module Kiba
               files: {
                 source: :prep__loan,
                 destination: :review__outgoing_loan,
-                lookup: %i[
-                  prep__loan_attachment
-                  loan__target_system_lookup
+                lookup: [
+                  :prep__loan_attachment,
+                  {
+                    jobkey: :preprocess__loan_catalog_items,
+                    lookup_on: :loanid
+                  }
                 ]
               },
               transformer: [xforms, Ppwe::Review.final_xforms].compact
@@ -25,19 +28,11 @@ module Kiba
             Kiba.job_segment do
               transform Delete::Fields, fields: :loannumberandrecipient
 
-              transform Merge::MultiRowLookup,
-                lookup: loan__target_system_lookup,
-                keycolumn: :id,
-                fieldmap: {
-                  Ppwe::Splitting.item_type_field =>
-                    Ppwe::Splitting.item_type_field
-                }
-
-              transform Deduplicate::FieldValues,
-                fields: Ppwe::Splitting.item_type_field,
-                sep: Ppwe.delim
-
-              transform Ppwe::Transforms::ReviewTargetFieldMerger
+              transform Ppwe::Transforms::MergeTable,
+                source: :loan__target_system_lookup,
+                join_column: :id,
+                delete_join_column: false,
+                drop_fields: :loannumberandrecipient
 
               transform Ppwe::Transforms::MergeTable,
                 source: :prep__loan_insurance_information,
@@ -53,6 +48,11 @@ module Kiba
                 source: :prep__loan_contact_information,
                 join_column: :id,
                 delete_join_column: false
+
+              transform Count::MatchingRowsInLookup,
+                lookup: preprocess__loan_catalog_items,
+                keycolumn: :id,
+                targetfield: :numberofcatalogitems
 
               transform Count::MatchingRowsInLookup,
                 lookup: prep__loan_attachment,
