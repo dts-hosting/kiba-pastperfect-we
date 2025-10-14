4534b65a31adf9649d477dbd60d9696b2830d3cf
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-14 15:54:06 -0400

    feat: Include loannumber in accession_target_system_lookup.number

--- lib/kiba/pastperfect_we/jobs/accession/target_system_lookup.rb
+++ lib/kiba/pastperfect_we/jobs/accession/target_system_lookup.rb
@@ -21,7 +21,13 @@ module Kiba
           def xforms
             Kiba.job_segment do
               transform Delete::FieldsExcept,
-                fields: %i[id number accessiontype]
+                fields: %i[id number accessiontype loannumber]
+              transform CombineValues::FromFieldsWithDelimiter,
+                sources: %i[number loannumber],
+                target: :number,
+                delete_sources: true,
+                delim: "; "
+
               transform Merge::MultiRowLookup,
                 lookup: prep__catalog_item,
                 keycolumn: :id,
