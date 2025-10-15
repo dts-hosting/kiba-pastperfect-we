b0b9964c6f76bd4145613cf018d48a2ae87356b3
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-15 19:31:47 -0400

    feat: Add prep__catalog_item_notes_and_legal

---
+++ lib/kiba/pastperfect_we/jobs/prep/catalog_item_notes_and_legal.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module Prep
+        module CatalogItemNotesAndLegal
+          module_function
+
+          def job(source:, dest:)
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: source,
+                destination: dest
+              },
+              transformer: Ppwe::Prep.get_xforms(self)
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              # if Ppwe.mode == :review
+              #   transform Replace::FieldValueWithStaticMapping,
+              #     source: :webright,
+              #     mapping: Ppwe::Enums.web_right,
+              #     fallback_val: nil
+              # end
+            end
+          end
+        end
+      end
+    end
+  end
+end
