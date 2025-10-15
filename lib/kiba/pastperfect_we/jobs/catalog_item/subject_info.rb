d2ddcc86f0312adb3743fff605ad2af4d6d47657
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-15 19:58:51 -0400

    feat: Merge person into catalog_item__subject_info

--- lib/kiba/pastperfect_we/jobs/catalog_item/subject_info.rb
+++ lib/kiba/pastperfect_we/jobs/catalog_item/subject_info.rb
@@ -13,6 +13,7 @@ module Kiba
                 source: :catalog_item__base,
                 destination: :catalog_item__subject_info,
                 lookup: %i[
+                  prep__catalog_item_people
                   prep__catalog_item_classification
                   prep__catalog_item_subjects
                   prep__catalog_item_tags
@@ -24,6 +25,12 @@ module Kiba
 
           def xforms
             Kiba.job_segment do
+              transform Merge::MultiRowLookup,
+                lookup: prep__catalog_item_people,
+                keycolumn: :catalogitemid,
+                fieldmap: {person: :person_name},
+                sorter: Lookup::RowSorter.new(on: :position, as: :to_i)
+
               transform Merge::MultiRowLookup,
                 lookup: prep__catalog_item_classification,
                 keycolumn: :catalogitemid,
@@ -42,7 +49,7 @@ module Kiba
                 fieldmap: {tags: :tags},
                 sorter: Lookup::RowSorter.new(on: :position, as: :to_i)
 
-              content_fields = %i[classification subjects tags]
+              content_fields = %i[person classification subjects tags]
               transform FilterRows::AnyFieldsPopulated,
                 action: :keep,
                 fields: content_fields
