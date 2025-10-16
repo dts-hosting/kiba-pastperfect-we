dde2c8c56a0f0b02f81ce4ce42563ba6114a6e5b
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-15 20:39:13 -0400

    feat: Add catalog_item__natural_history

---
+++ lib/kiba/pastperfect_we/jobs/catalog_item/natural_history.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Kiba
+  module PastperfectWe
+    module Jobs
+      module CatalogItem
+        module NaturalHistory
+          module_function
+
+          def job
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: :catalog_item__base,
+                destination: :catalog_item__natural_history
+              },
+              transformer: [xforms, Ppwe::Review.final_xforms].compact
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Ppwe::Transforms::MergeTable,
+                source: :prep__catalog_item_natural_history,
+                join_column: :catalogitemid,
+                delete_join_column: false
+
+              content_fields = Ppwe.mergeable_headers_for(
+                :prep__catalog_item_natural_history
+              )
+
+              transform FilterRows::AnyFieldsPopulated,
+                action: :keep,
+                fields: content_fields
+            end
+          end
+        end
+      end
+    end
+  end
+end
