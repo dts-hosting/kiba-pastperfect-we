923de2268f79893f54fc75c82cded029419f1881
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-10-27 16:36:53 -0400

    feat: Finalize review__location
    Resolves #92

--- lib/kiba/pastperfect_we/jobs/review/location.rb
+++ lib/kiba/pastperfect_we/jobs/review/location.rb
@@ -11,7 +11,11 @@ module Kiba
             Kiba::Extend::Jobs::Job.new(
               files: {
                 source: :prep__location,
-                destination: :review__location
+                destination: :review__location,
+                lookup: %i[
+                  location__prefixed
+                  target_system_lookup__location
+                ]
               },
               transformer: [xforms, Ppwe::Review.final_xforms].compact
             )
@@ -19,8 +23,41 @@ module Kiba
 
           def xforms
             Kiba.job_segment do
-              transform Ppwe::Transforms::DeleteTermSourceIndication,
-                table: "Location"
+              transform Merge::MultiRowLookup,
+                lookup: location__prefixed,
+                keycolumn: :id,
+                fieldmap: {location: :location}
+              transform Deduplicate::Table,
+                field: :location,
+                compile_uniq_fieldvals: true,
+                compile_delim: Ppwe.delim,
+                include_occs: true
+              transform Merge::MultiRowLookup,
+                lookup: target_system_lookup__location,
+                keycolumn: :id,
+                fieldmap: {
+                  Ppwe::Splitting.item_type_field =>
+                    Ppwe::Splitting.item_type_field
+                },
+                multikey: true
+              transform Deduplicate::FieldValues,
+                fields: Ppwe::Splitting.item_type_field,
+                sep: Ppwe.delim
+              transform Ppwe::Transforms::ReviewTargetFieldMerger
+
+              fields = Ppwe::Jobs::Location::Prefixed.field_name_lookup.keys
+              transform CombineValues::FromFieldsWithDelimiter,
+                sources: fields,
+                target: :displaylocation,
+                delete_sources: false,
+                delim: ", "
+              transform Sort::ByFieldValue,
+                field: :displaylocation,
+                mode: :string
+              transform Deduplicate::FlagAll,
+                on_field: :displaylocation,
+                in_field: :duplicatedisplayloc,
+                explicit_no: false
             end
           end
         end
